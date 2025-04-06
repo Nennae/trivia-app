58e1767b0956ffc3fa714220e54bff85fa6a997c
Nennae <sezenerdal@outlook.com>
2025-04-06 19:43:06 +0200

    added card with play button

--- app/components/cards/start-card.tsx
+++ app/components/cards/start-card.tsx
@@ -1,8 +1,10 @@
+import Button from "../buttons/button";
 
 export default function StartCard() {
   return (
-    <div>
-      
+    <div className="card flex-center-column">
+      <p className="text-lg text-center font-semibold">Test your knowledge with 10 random questions!</p>
+      <Button></Button>
     </div>
-  )
+  );
 }
