0a4d4c3faccc074975031f7935eec0a9d82a701d
Nennae <sezenerdal@outlook.com>
2025-04-07 00:54:16 +0200

    fetched questions from API

--- app/components/cards/question-card.tsx
+++ app/components/cards/question-card.tsx
@@ -1,8 +1,18 @@
+"use client";
 
-export default function QuestionCard() {
-  return (
-    <div>
-      
-    </div>
-  )
+import { Questions } from "@/interfaces/questions-interface";
+import { decode } from "he";
+
+interface Props {
+  question: Questions;
 }
+
+export default function QuestionCard({ question }: Props): React.JSX.Element {
+ 
+  return (
+   <div className="border rounded-xl shadow p-4 bg-white">
+      <h2 className="text-lg font-semibold mb-3 text-black">{decode(question.question)}</h2>
+    </div>
+  );
+}
+
