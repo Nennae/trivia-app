0504b67ea1c0575b2f95e2a60b4ab49ad25081b8
Nennae <sezenerdal@outlook.com>
2025-04-07 16:40:48 +0200

    renamed cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import Card from "./components/cards/start-card";
+import StartCard from "./components/cards/start-card";
 
 export default function Home() {
   return (
     <div>
       <main className="flex-center-column">
         <h2>Play Trivia</h2>
-        <Card></Card>
+        <StartCard></StartCard>
       </main>
     </div>
   );
