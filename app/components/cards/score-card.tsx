eb59910e6800d197ff67deef3c5684209343c998
Nennae <sezenerdal@outlook.com>
2025-04-14 00:23:44 +0200

    rendered the final score of played game

--- app/components/cards/score-card.tsx
+++ app/components/cards/score-card.tsx
@@ -1,11 +1,16 @@
+"use client";
+
+import { useQuiz } from '@/context/QuizContext';
 import Button from '../button/button';
 
 export default function ScoreCard() {
+  const { getScore, questions } = useQuiz();
+
   return (
     <div className="card">
       <h4>You scored:</h4>
-      <h4 className="font-bold">10 / 20</h4>
+      <h4 className="font-bold">{getScore()} / {questions.length}</h4>
       <Button />
     </div>
-  )
+  );
 }
