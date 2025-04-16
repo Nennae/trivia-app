05defe64a1420128671070a20ffce3e7427b2164
Nennae <sezenerdal@outlook.com>
2025-04-16 19:51:30 +0200

    added styling for light mode

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -8,12 +8,22 @@ export default function Header() {
   return (
     <div>
       <header className="header">
-        <Link className="flex" href="/">
+        <Link className="flex items-center" href="/">
+          {/* Light mode logo */}
           <Image
-            src="/assets/logo/quizlet-icon.svg"
-            alt="q icon"
+            src="/assets/logo/quizlet-icon-light.svg"
+            alt="q icon light"
             width={50}
             height={50}
+            className="hidden dark:block"
+          />
+          {/* Dark mode logo */}
+          <Image
+            src="/assets/logo/quizlet-icon-dark.svg"
+            alt="q icon dark"
+            width={50}
+            height={50}
+            className="block dark:hidden"
           />
           <h1>uiz</h1>
         </Link>
