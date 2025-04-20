a2d7f8ea06ec6f0024d793ec722b35e0a97d9dc5
Nennae <sezenerdal@outlook.com>
2025-04-20 23:36:42 +0200

    rendered categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import StartCard from "./components/cards/start-card";
+import CategoryPanel from "./components/category-panel/category-panel";
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
       <main className="flex-center-column">
         <h2>Play Trivia</h2>
         <StartCard></StartCard>
+        <CategoryPanel name={[]} />
       </main>
     </div>
   );
